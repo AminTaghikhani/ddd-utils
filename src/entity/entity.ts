1d6d35dc62d14e58ac64e39878850b290d37af29
Amin Taghikhani <amin76taghikhani@gmail.com>
2022-06-10 18:40:55 +0430

    update entity

--- src/entity/entity.ts
+++ src/entity/entity.ts
@@ -1,19 +1,32 @@
 import { UUIDIdentifier } from '../identifier';
-import { IEqualable } from '../utils';
+import { BaseDate, IEqualable, JsDate } from '../utils';
 import * as lodash from 'lodash';
 
-export abstract class DomainEntity implements IEqualable<DomainEntity> {
+export abstract class DomainEntity<PropertyInterface>
+  implements IEqualable<DomainEntity<PropertyInterface>>
+{
   protected readonly _id: UUIDIdentifier;
+  protected readonly _createdAt: BaseDate;
+  protected readonly _updatedAt: BaseDate;
+  protected _props: PropertyInterface;
 
-  protected constructor(id: UUIDIdentifier) {
-    this._id = id;
+  protected constructor(
+    props: PropertyInterface,
+    id?: UUIDIdentifier,
+    createdAt?: BaseDate,
+    updatedAt?: BaseDate,
+  ) {
+    this._props = props;
+    this._id = id || new UUIDIdentifier();
+    this._createdAt = createdAt || new JsDate();
+    this._updatedAt = updatedAt || new JsDate();
   }
 
   get id(): UUIDIdentifier {
     return this._id;
   }
 
-  toEqual(other: DomainEntity): boolean {
+  toEqual(other: DomainEntity<PropertyInterface>): boolean {
     return lodash.isEqual(this, other);
   }
 }
