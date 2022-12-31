41f965b854c5d89a0a19d99385f5b4267af2e109
Amin Taghikhani <amin76taghikhani@gmail.com>
2022-12-31 16:22:33 +0330

    add action type

---
+++ src/utils/action.ts
@@ -0,0 +1,1 @@
+export type Action<Type> = (action: Type) => void;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './action';
 export * from './comparable';
 export * from './equalable';
 export * from './nullable';
