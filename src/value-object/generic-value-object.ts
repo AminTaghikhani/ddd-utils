d732fbda1b335957a773dcb1441119e2dd781753
Amin Taghikhani <amin76taghikhani@gmail.com>
2023-01-01 22:56:07 +0330

    add validate and parse to generic value object

--- src/value-object/generic-value-object.ts
+++ src/value-object/generic-value-object.ts
@@ -3,9 +3,15 @@ import { ValueObject } from './value-object';
 export abstract class GenericValueObject<T> extends ValueObject {
   protected _value: T;
 
-  protected constructor(value: T) {
+  protected constructor(value: unknown) {
     super();
-    this._value = value;
+    this._value = this.parseValue(value);
+    this.validate();
+  }
+
+  protected abstract parseValue(value: unknown): T;
+  protected validate(): void {
+    // do nothing
   }
 
   get value(): T {
